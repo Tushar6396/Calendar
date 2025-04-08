ce9b12fdaa414640e0f75bdead01246449ae6578
Tushar <111505346+Tushar6396@users.noreply.github.com>
2025-04-08 12:51:06 +0530

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: 'https://calendar-1-0rjy.onrender.com' || 'http://localhost:5173',
+    origin: ['https://calendar-1-0rjy.onrender.com', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
